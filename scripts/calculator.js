9164a094f51df2414e3923806bb2dbc1b8fc73fc
SarrikGmm0 <vladgimazov461@gmail.com>
2025-03-01 20:15:35 +0200

    Update calculator.js

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -4,23 +4,25 @@ function calculateMmr() {
     let deaths = parseInt(document.getElementById("deaths").value) || 0;
     let kd = deaths === 0 ? kills : kills / deaths;
     let mmr = 0;
-
-    if (kills === 0 && deaths === 0) {
-        document.getElementById("mmr").innerText = "Вы не ввели данные!";
-        document.getElementById("kdValue").innerText = "";
-        return;
-    }
+	
+	if (kills === 0 && deaths === 0) {
+    document.getElementById("mmr").innerText = "Вы не ввели данные!";
+    document.getElementById("kdValue").innerText = "";
+    return;
+}
 
     if (result === "win") {
-        if (kd >= 2) mmr = Math.floor(Math.random() * (99 - 80 + 1)) + 80;
-        else if (kd >= 1) mmr = Math.floor(Math.random() * (79 - 60 + 1)) + 60;
-        else mmr = Math.floor(Math.random() * (59 - 40 + 1)) + 40;
+        if (kd >= 2) mmr = Math.floor(Math.random() * (80 - 60 + 1)) + 60;
+        else if (kd >= 1.5) mmr = Math.floor(Math.random() * (60 - 40 + 1)) + 40;
+        else if (kd >= 1) mmr = Math.floor(Math.random() * (40 - 20 + 1)) + 20;
+        else mmr = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
     } else {
-        if (kd >= 2) mmr = Math.floor(Math.random() * (51 - 70 + 1)) + 70;
-        else if (kd >= 1) mmr = Math.floor(Math.random() * (31 - 50 + 1)) + 50;
-        else mmr = Math.floor(Math.random() * (11 - 30 + 1)) + 30;
+        if (kd >= 2) mmr = -(Math.floor(Math.random() * (20 - 1 + 1)) + 1);
+        else if (kd >= 1.5) mmr = -(Math.floor(Math.random() * (40 - 20 + 1)) + 20);
+        else if (kd >= 1) mmr = -(Math.floor(Math.random() * (60 - 40 + 1)) + 40);
+        else mmr = -(Math.floor(Math.random() * (80 - 60 + 1)) + 60);
     }
 
-    document.getElementById("kdValue").innerText = "Ваш K/D: " + kd.toFixed(2);
     document.getElementById("mmr").innerText = "Ваш ММР: " + mmr;
+	document.getElementById("kdValue").innerText = "Ваш K/D: " + kd.toFixed(2);
 }
