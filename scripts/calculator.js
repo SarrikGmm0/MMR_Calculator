0d9b2b48e83d3f82cb8ba7dbbb3ad1fae50640e2
SarrikGmm0 <vladgimazov461@gmail.com>
2025-03-01 19:07:20 +0200

    Update calculator.js

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,20 @@
+function calculateMmr() {
+    let result = document.getElementById("result").value;
+    let kills = parseFloat(document.getElementById("kills").value);
+    let deaths = parseFloat(document.getElementById("deaths").value);
+    let kd = (deaths === 0) ? kills : (kills / deaths).toFixed(2);
+    let mmr = 0;
 
+    if (result === "win") {
+        if (kd >= 2) mmr = Math.floor(Math.random() * (99 - 80 + 1)) + 80;
+        else if (kd >= 1) mmr = Math.floor(Math.random() * (79 - 60 + 1)) + 60;
+        else mmr = Math.floor(Math.random() * (59 - 40 + 1)) + 40;
+    } else {
+        if (kd >= 2) mmr = Math.floor(Math.random() * (51 - 70 + 1)) + 70;
+        else if (kd >= 1) mmr = Math.floor(Math.random() * (31 - 50 + 1)) + 50;
+        else mmr = Math.floor(Math.random() * (11 - 30 + 1)) + 30;
+    }
+
+    document.getElementById("resultMmr").innerText = "Ваш MMR: " + mmr;
+    document.getElementById("resultKd").innerText = "Ваш K/D: " + kd;
+}
