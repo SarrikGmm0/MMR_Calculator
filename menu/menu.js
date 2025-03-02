8534860cdb1000899abcd4af4f55f7b898fe949f
SarrikGmm0 <vladgimazov461@gmail.com>
2025-03-02 11:36:26 +0200

    Update menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -1,17 +1,9 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
+function openMenu() {
+  const menu = document.getElementById("menu");
+  menu.style.display = "flex";
+}
 
-const Menu = ({ onClose }) => {
-  return (
-    <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white rounded-2xl p-8 shadow-lg grid gap-4">
-        <Button className="w-full" onClick={() => console.log("Настройки")}>Настройки</Button>
-        <Button className="w-full" onClick={() => console.log("О нас")}>О нас</Button>
-        <Button className="w-full" onClick={() => console.log("Выход")}>Выход</Button>
-        <Button className="w-full" onClick={onClose}>Закрыть</Button>
-      </div>
-    </div>
-  );
-};
-
-export default Menu;
+function closeMenu() {
+  const menu = document.getElementById("menu");
+  menu.style.display = "none";
+}
