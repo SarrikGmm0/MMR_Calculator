fef809ed44c726249abcea777f00dd8b575c9f44
SarrikGmm0 <vladgimazov461@gmail.com>
2025-03-02 11:41:01 +0200

    Update menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -1,9 +1,1 @@
-function openMenu() {
-  const menu = document.getElementById("menu");
-  menu.style.display = "flex";
-}
 
-function closeMenu() {
-  const menu = document.getElementById("menu");
-  menu.style.display = "none";
-}
